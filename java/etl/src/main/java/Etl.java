11a21d321778b6e111dd6bb3f6ed81bf87ac0eee
Urmil Narang <nurmil@thoughtworks.com>
2016-12-29 01:42:39 +0530

    [Urmil] add transform exercise

--- java/etl/src/main/java/Etl.java
+++ java/etl/src/main/java/Etl.java
@@ -1,8 +1,24 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Etl {
-    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-        return null;
+  public Map<String, Integer> transform(Map<Integer, List<String>> old) {
+
+    Map<String, Integer> modified = new HashMap<>();
+
+    for (Entry<Integer, List<String>> entry : old.entrySet()) {
+      addToModifiedMap(modified, entry.getKey(), entry.getValue());
     }
+
+    return modified;
+  }
+
+  private void addToModifiedMap(Map<String, Integer> modified, Integer point, List<String> stringList) {
+
+    for (int i = 0; i < stringList.size(); i++) {
+      modified.put(stringList.get(i).toLowerCase(), point);
+    }
+  }
 }
